880d1ac98a194c74c70d97874d765ecc5bbd33f4
Roberto Polli <roberto.polli@par-tec.it>
2016-06-16 17:55:29 +0200

    improved nosetests section

--- python-for-sysadmin/scripts/02_nosetests_full.py
+++ python-for-sysadmin/scripts/02_nosetests_full.py
@@ -4,32 +4,62 @@
 """
 
 import os
+import errno
 
+# Import assert_* tools
+from nose.tools import *
 
-class TestB:
+class TestB(object):
 
     @classmethod
     def setup_class(self):
         # Run once at startup, eg. create database structure
-        print("setup testsuite environment")
+        print("\nsetup testsuite environment")
         open("/tmp/test2.out", "w").write("0")
 
     @classmethod
     def teardown_class(self):
         # Run once at teardown, eg. drop database
-        print("cleanup testsuite environment")
+        print("\ncleanup testsuite environment")
         os.unlink("/tmp/test2.out")
 
     def setup(self):
         # Before every test, like populate a table
-        print("before_every_test")
+        print("\nbefore_every_test")
 
     def teardown(self):
         # After every test, eg truncate a table
-        print("after_every_test")
+        print("\nafter_every_test")
 
     def test_a(self):
         assert os.path.isfile("/tmp/test2.out")
 
     def test_b(self):
         assert False
+
+    #
+    # Bonus examples:
+    #
+    #   - testing exceptions
+    #   - parametrized tests 
+    #
+    @raises(IOError)
+    def test_except(self):
+        assert open('/tmp/MISSING')
+
+    def test_except_complex(self):
+        with assert_raises(IOError) as ex:
+            fh = open('/tmp/MISSING')
+        # Verify exception *and* errno!
+        assert_equals(ex.exception.errno, errno.ENOENT)
+
+    def test_parametrized(self):
+        # Runs 3 tests using the nosetest generator syntax.
+        for i in range(3):
+            # You have to yield a method to make it work into 
+            #  a class
+            yield self.harn_greater, i, 0 
+
+    def harn_greater(self, a, b):
+        assert_greater(a, b)
+
