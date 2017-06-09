e96394da95b66ff7b3ac3491f01f6045b30fdffe
Roberto Polli <roberto.polli@par-tec.it>
2017-06-10 00:07:39 +0200

    First stub

---
+++ ansible-101/notebooks/exercise-04/inventory-docker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# List our containers. Note: this only works with docker-compose containers.
+from __future__ import print_function
+from collections import defaultdict
+import json
+import docker
+import logging
+log = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
+c=docker.Client(base_url="http://172.17.0.1:2375")
+container_fmt = lambda x: (
+    x['Names'][0][1:],
+    x['Labels']['com.docker.compose.service'], 
+    x['NetworkSettings']['Networks']['bridge']['IPAddress'],
+)
+
+inventory = defaultdict(list)
+
+for x in c.containers():
+    log.debug("Processing entry %r", '\t\t'.join(container_fmt(x)))
+    group_name = x['Labels']['com.docker.compose.service']
+    ip_address = x['NetworkSettings']['Networks']['bridge']['IPAddress']
+    inventory[group_name].append(ip_address)
+
+inventory['web']['ansible_ssh_common_args'] = ' -o StrictHostKeyChecking=no '
+print(json.dumps(inventory, indent=True))
