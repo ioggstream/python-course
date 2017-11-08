869f431f3e35a24e627e841dfcc456a5bbf2737f
Roberto Polli <robipolli@gmail.com>
2017-11-08 14:55:56 +0100

    Move 04_inventories to 05_inventories.

--- ansible-101/notebooks/exercise-09/ansible.cfg.sol
+++ ansible-101/notebooks/exercise-09/ansible.cfg.sol
@@ -1,5 +1,5 @@
 [defaults]
-inventory = ../exercise-04/inventory-docker-solution.py
+inventory = ../exercise-05/inventory-docker-solution.py
 private_key_file = ../exercise-01/id_ansible
 retry_files_enabled = no
 host_key_checking = no
