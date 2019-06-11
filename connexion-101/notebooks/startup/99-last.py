48cdd51766c1b14e678bec5471bf8f113c03c053
Roberto Polli <robipolli@gmail.com>
2019-06-11 23:53:03 +0200

    Basic connexion

--- connexion-101/notebooks/startup/99-last.py
+++ connexion-101/notebooks/startup/99-last.py
@@ -4,6 +4,17 @@ from os import environ as env
 import os
 import sys
 from IPython.display import display, Markdown, Latex
+import yaml
+import json
 
 def render_markdown(s):
     return display(Markdown(s))
+
+#
+# Useful constants
+#
+editor_url = f'http://{env["MAIN_IP"]}'+':8080/?url=' 
+
+def oas_editor_url(url):
+    return editor_url + url
+
