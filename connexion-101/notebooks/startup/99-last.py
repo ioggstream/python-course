1b3043a207b8e668baa5467de928f05342ef5014
Roberto Polli <robipolli@gmail.com>
2019-06-11 19:54:01 +0200

    OAS3

---
+++ connexion-101/notebooks/startup/99-last.py
@@ -0,0 +1,20 @@
+# Load default libraries and functions in jupyter
+
+from os import environ as env
+import os
+import sys
+from IPython.display import display, Markdown, Latex
+import yaml
+import json
+
+def render_markdown(s):
+    return display(Markdown(s))
+
+#
+# Useful constants
+#
+editor_url = f'http://{env["MAIN_IP"]}'+':8080/?url=' 
+
+def oas_editor_url(url):
+    return editor_url + url
+
