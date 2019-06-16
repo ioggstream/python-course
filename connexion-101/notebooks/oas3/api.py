7f7a57d78ed6f0a9e1b7e428ae221a685073795d
Roberto Polli <robipolli@gmail.com>
2019-06-16 17:20:09 +0200

    Implementing functions.

---
+++ connexion-101/notebooks/oas3/api.py
@@ -0,0 +1,10 @@
+from connexion import problem
+
+def get_status():
+    """Implement the get_status operation
+    :return: a problem+json with status 200, title "OK" and a successful
+             message in detail
+    """
+
+    raise NotImplementedError
+
