37e1508a4c2ea67c09a7280ffe0656cc27dd5183
Roberto Polli <robipolli@gmail.com>
2019-06-23 15:00:52 +0200

    basic security

---
+++ connexion-101/notebooks/oas3/security.py
@@ -0,0 +1,14 @@
+"""
+A file implementing securitySchemes
+"""
+
+def my_auth(username, password,required_scopes=None):
+    """An dummy authentication function.
+       :params: username, the username
+       :params: password, the password
+       :params: scopes, the scope
+       :returns: `{"sub": username, "scope": ""}` on success, 
+                 None on failure
+    """   
+    raise NotImplementedError
+
