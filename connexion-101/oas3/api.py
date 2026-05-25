27f6e17cd7a1f5638dd97f2b77f289f16bdd32e8
ioggstream <robipolli@gmail.com>
2026-05-25 10:15:27 +0000

    Deploying to gh-pages from @ ioggstream/python-course@ff24e9d8df56f518502230b767127c4640a62337 🚀

---
+++ connexion-101/oas3/api.py
@@ -0,0 +1,20 @@
+def _get_status():
+    """Implement the get_status operation
+    :return: a problem+json with status 200, title "OK" and a successful
+             message in detail
+    """
+    raise NotImplementedError
+
+
+def get_echo(tz="Zulu", user=None, token_info=None):
+    raise NotImplementedError
+
+
+def get_timezones(limit=5, offset=0, continent=None):
+    raise NotImplementedError
+    return {"limit": limit, "offset": offset, "entries": entries, "count": len(entries)}
+
+
+# @throttle
+def get_timezones_by_continent(limit=5, offset=0, continent=None):
+    return get_timezones(limit, offset, continent)
